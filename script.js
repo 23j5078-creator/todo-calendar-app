b8bab0aeacf7e4a8113cb87aecb89ffd4407ed82
谷川和馬 <23j5078@stu.meisei-u.ac.jp>
2025-11-17 15:19:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -520,7 +520,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const details = encodeURIComponent("ToDoアプリから追加");
         const dates = `${startStr}/${endStr}`;
         const url = `https://calendar.google.com/calendar/render?action=TEMPLATE&text=${title}&dates=${dates}&details=${details}&sf=true&output=xml`;
-        window.open(url, "_blank");
+        window.location.href = url;
       });
       right.appendChild(calBtn);
     }
